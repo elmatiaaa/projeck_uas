f8a9be0bd859ea966962fd180c5cf671e7ba4fa6
elmatiaaa <112562792+elmatiaaa@users.noreply.github.com>
2023-06-15 20:50:56 +0700

    Update index.py

--- index.py
+++ index.py
@@ -61,7 +61,7 @@ with preprocessing:
     scaler = MinMaxScaler()
     #scaler.fit(features)
     #scaler.transform(features)
-    scaled = scaler.fit_transform(X)
+    scaler = scaler.fit_transform(X)
     features_names = X.columns.copy()
     #features_names.remove('label')
     scaled_features = pd.DataFrame(scaled, columns=features_names)
